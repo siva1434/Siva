159b0eba506ee208285b1e2114556c78b869e598
JCPW1003 <jay.ashara@techstern.com>
2018-09-08 11:30:05 +0530

    page remove

--- 360LawGroup.CostOfSalesBilling.Web/Areas/All/Controllers/ClientsController.cs
+++ 360LawGroup.CostOfSalesBilling.Web/Areas/All/Controllers/ClientsController.cs
@@ -92,32 +92,5 @@ namespace _360LawGroup.CostOfSalesBilling.Web.Areas.All.Controllers
             }
             return PartialView("Show", model);
         }
-
-
-        public ActionResult ClientRenewals()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult ClientRenewalsDetails(string id = null)
-        {
-            var model = new ClientViewModel();
-            if (!string.IsNullOrEmpty(id))
-            {
-                var status = ApiHelper.Get<ClientViewModel>("api/all/client/getbyid", id);
-
-                if (status.StatusCode == HttpStatusCode.OK)
-                {
-                    model.Id = status.Data.Id;
-                    model.Company = status.Data.Company;
-                    model.FullName = status.Data.FullName;
-                    model.LastName = status.Data.LastName;
-                    model.Email = status.Data.Email;
-                    model.JobTitle = status.Data.JobTitle;
-                }
-            }
-            return PartialView(model);
-        }
     }
 }
